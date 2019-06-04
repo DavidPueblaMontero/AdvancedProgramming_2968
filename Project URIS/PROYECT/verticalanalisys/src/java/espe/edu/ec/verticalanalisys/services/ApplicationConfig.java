90361ad1d6aec7297dfc0c45ea0230e51e772fc7
David Alexander Puebla Montero <david_puebla20@hotmail.com>
2019-06-03 23:14:51 -0500

    changed v1 to data

--- Project URIS/PROYECT/verticalanalisys/src/java/espe/edu/ec/verticalanalisys/services/ApplicationConfig.java
+++ Project URIS/PROYECT/verticalanalisys/src/java/espe/edu/ec/verticalanalisys/services/ApplicationConfig.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Application;
  *
  * @author jorge, jess, david
  */
-@javax.ws.rs.ApplicationPath("v1")
+@javax.ws.rs.ApplicationPath("data")
 public class ApplicationConfig extends Application {
 
     @Override
