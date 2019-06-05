43f2ab1bd093e3efbb2df86703f0bce56e68bd14
David Alexander Puebla Montero <david_puebla20@hotmail.com>
2019-06-05 07:51:16 -0500

    Correction of UpdateForm

--- Project URIS/Forms/Users/updateUserForm.php
+++ Project URIS/Forms/Users/updateUserForm.php
@@ -7,7 +7,7 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>Register User - Formoid javascript form</title>
+	<title>Update User - Formoid javascript form</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body class="blurBg-false" style="background-color:#EBEBEB">
@@ -17,7 +17,7 @@
 <!-- Start Formoid form-->
 <link rel="stylesheet" href="registeruser_files/formoid1/formoid-solid-blue.css" type="text/css" />
 <script type="text/javascript" src="registeruser_files/formoid1/jquery.min.js"></script>
-<form action="updateUri.php?id=<?php echo $id?>" class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Register User</h2></div>
+<form action="updateUri.php?id=<?php echo $id?>" class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Update User</h2></div>
 	<div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="id" placeholder="Id User" value="<?php echo $id;?>" disabled/><span class="icon-place"></span></div></div>
 	<div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="name" placeholder="Name User"/><span class="icon-place"></span></div></div>
 	<div class="element-password"><label class="title"></label><div class="item-cont"><input class="large" type="password" name="password" value="" placeholder="Password"/><span class="icon-place"></span></div></div>
@@ -33,7 +33,7 @@
         ?> 
 		
 	</select><i></i><span class="icon-place"></span></span></div></div></div>
-<div class="submit"><input type="submit" value="Register"/></div></form><p class="frmd"><a href="http://formoid.com/v29.php">javascript form</a> Formoid.com 2.9</p><script type="text/javascript" src="registeruser_files/formoid1/formoid-solid-blue.js"></script>
+<div class="submit"><input type="submit" value="Update"/></div></form><p class="frmd"><a href="http://formoid.com/v29.php">javascript form</a> Formoid.com 2.9</p><script type="text/javascript" src="registeruser_files/formoid1/formoid-solid-blue.js"></script>
 <!-- Stop Formoid form-->
 
 
