44b42a691fe854b79a424855980225c441851baa
JORGE REYES <jorgereyesn96@hotmail.com>
2019-06-05 12:06:08 -0500

    Add Financial Report

---
+++ Project URIS/Forms/Companies/reportCompany.php
@@ -0,0 +1,50 @@
+<?php
+
+$id_company = $_POST['id_company'];
+$year = $_POST['year_company'];
+$uri = "http://financialreport.ddns.net/verticalanalisys/data/companiesfinancialreport/$id_company/$year";
+$data = json_decode(file_get_contents($uri), true);
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Financial</title>
+</head>
+
+<body>
+    <h3 align="center">Report Company with id: <?php echo $id_company?></h3>
+    <table class="egt" border="1px" align="center">
+        <tr>
+             
+            <td>Sales</td>  
+            <td>Sales Cost</td>  
+            <td>Gross Profit</td>  
+            <td>Expenses Administrative Sales</td>  
+            <td>Depreciations</td>  
+            <td>Interest Paid</td>  
+            <td>Profit Before Taxes</td>  
+            <td>Taxes</td>  
+            <td>Exercise Utility</td>  
+        </tr>
+            <?php 
+            
+                
+                echo ("<td><p>{$data['sales']}</p></td>");
+                echo ("<td><p>{$data['salesCost']}</p></td>");
+                echo ("<td><p>{$data['grossProfit']}</p></td>");
+                echo ("<td><p>{$data['expensesAdmiSales']}</p></td>");
+                echo ("<td><p>{$data['depreciations']}</p></td>");
+                echo ("<td><p>{$data['interestPaid']}</p></td>");
+                echo ("<td><p>{$data['profitBeforeTaxes']}</p></td>");
+                echo ("<td><p>{$data['taxes']}</p></td>");
+                echo ("<td><p>{$data['exerciseUtility']}</p></td></tr>");
+        
+            
+            ?>
+    </table>
+</body>
+
+</html>
