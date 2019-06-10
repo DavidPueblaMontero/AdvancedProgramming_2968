10224742fe99ccf9a1fccf8ad31e756470ac3be5
David Alexander Puebla Montero <david_puebla20@hotmail.com>
2019-06-09 21:41:57 -0500

    Modified Files

---
+++ Project URIS/Forms/Companies/MenuCompany.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Company Options</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body class="blurBg-false" style="background-color:#EBEBEB" >
+
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="menucompany_files/formoid1/formoid-solid-blue.css" type="text/css" />
+<script type="text/javascript" src="menucompany_files/formoid1/jquery.min.js"></script>
+<form class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:26px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post" action="RegisterCompany.html"><div class="title" ><h2>Company Options</h2></div>
+<div class="submit"><input type="submit" value="Company Register"/></div></form><p class="frmd"><a href="http://formoid.com/v29.php">jquery form validation</a> Formoid.com 2.9</p><script type="text/javascript" src="menucompany_files/formoid1/formoid-solid-blue.js"></script>
+<!-- Stop Formoid form-->
+
+</body>
+
+<body class="blurBg-false" style="background-color:#EBEBEB">
+
+
+
+	<!-- Start Formoid form-->
+	<form class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:26px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post" action="searchCompany.php">
+	<div class="submit"><input type="submit" value="Search Company"/></div></form><p class="frmd"><a href="http://formoid.com/v29.php">jquery form validation</a> Formoid.com 2.9</p><script type="text/javascript" src="menucompany_files/formoid1/formoid-solid-blue.js"></script>
+	<!-- Stop Formoid form-->
+	
+	
+	
+</body>
+<body class="blurBg-false" style="background-color:#EBEBEB">
+
+
+
+		<!-- Start Formoid form-->
+		<form class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:26px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post" action="searchPutCompany.php">
+		<div class="submit"><input type="submit" value="Edit Company"/></div></form><p class="frmd"><a href="http://formoid.com/v29.php">jquery form validation</a> Formoid.com 2.9</p><script type="text/javascript" src="menucompany_files/formoid1/formoid-solid-blue.js"></script>
+		<!-- Stop Formoid form-->
+		
+		
+		
+</body>
+<body class="blurBg-false" style="background-color:#EBEBEB">
+
+
+
+		<!-- Start Formoid form-->
+		<form class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:26px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post" action="searchDeleteCompany.php">
+		<div class="submit"><input type="submit" value="Delete Company"/></div></form><p class="frmd"><a href="http://formoid.com/v29.php">jquery form validation</a> Formoid.com 2.9</p><script type="text/javascript" src="menucompany_files/formoid1/formoid-solid-blue.js"></script>
+		<!-- Stop Formoid form-->
+		
+		
+		
+</body>
+
+</html>
